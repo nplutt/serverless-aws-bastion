f3d805ff48199858f75fb27d9e72810e93366e65
Restyled.io <commits@restyled.io>
2020-12-06 22:18:14 +0000

    Restyled by shellharden

--- docker_files/boot.sh
+++ docker_files/boot.sh
@@ -7,16 +7,16 @@ cleanup() {
         sudo grep 'Successfully registered the instance with AWS SSM using Managed instance-id:' /var/log/amazon/ssm/amazon-ssm-agent.log \
         | awk '{print $NF}'
     )
-    aws ssm deregister-managed-instance --instance-id ${INSTANCE_ID} --region ${AWS_REGION}
+    aws ssm deregister-managed-instance --instance-id "$INSTANCE_ID" --region "$AWS_REGION"
 }
 trap cleanup EXIT SIGTERM SIGKILL
 
 echo "Adding ssh key to authorized keys..."
-echo ${AUTHORIZED_SSH_KEYS} >> /home/ssm-user/.ssh/authorized_keys
+echo "$AUTHORIZED_SSH_KEYS" >> /home/ssm-user/.ssh/authorized_keys
 
 echo "Starting ssh..."
 /usr/sbin/sshd -Dd -e4 -f /etc/ssh/sshd_config &
 
 echo "Registering & starting ssm..."
-/usr/bin/amazon-ssm-agent -register -code ${ACTIVATION_CODE} -id ${ACTIVATION_ID} -region ${AWS_REGION} -clear -y
+/usr/bin/amazon-ssm-agent -register -code "$ACTIVATION_CODE" -id "$ACTIVATION_ID" -region "$AWS_REGION" -clear -y
 /usr/bin/amazon-ssm-agent
