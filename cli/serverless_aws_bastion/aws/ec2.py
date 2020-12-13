fa8dc2ff5343326d3cd1e46e91e6f588d27d11e3
nplutt <nplutt@gmail.com>
2020-12-12 21:44:07 -0600

    undo docker build

--- cli/serverless_aws_bastion/aws/ec2.py
+++ cli/serverless_aws_bastion/aws/ec2.py
@@ -8,6 +8,10 @@ from serverless_aws_bastion.utils.aws_utils import fetch_boto3_client
 def load_public_ips_for_network_interfaces(
     interface_ids: List[str],
 ) -> List[str]:
+    """
+    Loads the public ip addresses for a list of network
+    interface ids
+    """
     client: EC2Client = fetch_boto3_client("ec2")
     interfaces = client.describe_network_interfaces(
         NetworkInterfaceIds=interface_ids,
